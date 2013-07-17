039712f0fe99608aeb317da06fc0e8f1f22bfcb5
Tomasz Kamiński <tomaszkam@gmail.com>
2013-07-17 07:44:36 +0200

    Extracted viable reference implementation.
    Extracted viable reference getter implementation to separate file named viable_references.hpp.

---
+++ invoke/viable_reference.hpp
@@ -0,0 +1,110 @@
+#ifndef VIABLE_REF_HPP
+#define VIABLE_REF_HPP
+
+#include <tuple>
+
+namespace functional
+{
+  namespace detail
+  {
+    template<typename Class, typename Member>
+    struct viable_references_impl
+    {
+      typedef std::tuple<
+        Class&, Class&&,
+        Class const&, Class const&&,
+        Class volatile&, Class volatile&&,
+        Class const volatile&, Class const volatile&&
+      > type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...)>
+    {
+      typedef std::tuple<Class&, Class&&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) const>
+    {
+      typedef std::tuple<Class const&, Class const&&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) volatile>
+    {
+      typedef std::tuple<Class volatile&, Class volatile&&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) const volatile>
+    {
+      typedef std::tuple<Class const volatile&, Class const volatile&&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) &>
+    {
+      typedef std::tuple<Class&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) const&>
+    {
+      typedef std::tuple<Class const&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) volatile&>
+    {
+      typedef std::tuple<Class volatile&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) const volatile&>
+    {
+      typedef std::tuple<Class const volatile&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) &&>
+    {
+      typedef std::tuple<Class&&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) const&&>
+    {
+      typedef std::tuple<Class const&&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) volatile&&>
+    {
+      typedef std::tuple<Class volatile&&> type;
+    };
+
+    template<typename Class, typename Ret, typename... Args>
+    struct viable_references_impl<Class, Ret(Args...) const volatile&&>
+    {
+      typedef std::tuple<Class const volatile&&> type;
+    };
+  }
+
+  template<typename T>
+  struct viable_references
+  {
+    typedef std::tuple<> type;
+  };
+
+  template<typename Class, typename Member>
+  struct viable_references<Member Class::*>
+  {
+    typedef typename detail::viable_references_impl<Class, Member>::type type;
+  };
+
+  template<typename T>
+  using viable_references_t = typename viable_references<T>::type;
+}
+
+#endif //VIABLE_REF_HPP
