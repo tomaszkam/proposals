33b022ec0040b9ceb339540f55681300fcb81d97
Tomasz Kamiński <tomaszkam@gmail.com>
2013-07-18 13:38:23 +0200

    Add implementation of C++11 compatible invoke.
    Add implementation of C++11 standard comformant invoke function.

---
+++ invoke/invoke_cpp11.hpp
@@ -0,0 +1,89 @@
+#ifndef INVOKE_CPP11_HPP
+#define INVOKE_CPP11_HPP
+
+#include "type_traits.hpp"
+#include "to_viable_reference.hpp"
+
+namespace functional
+{
+  namespace cpp11
+  {
+    template<typename Functor, typename Object, typename... Args>
+    constexpr auto invoke(Functor&& functor, Object&& object, Args&&... args)
+      ->  typename std::enable_if<
+            std::is_member_function_pointer<
+              typename std::decay<Functor>::type
+            >::value &&
+            type_traits::is_target_reference<
+              Object&&,
+              typename std::decay<Functor>::type
+            >::value,
+            decltype((object.*functor)(std::forward<Args>(args)...))
+          >::type
+    {
+      return (object.*functor)(std::forward<Args>(args)...);
+    }
+
+    template<typename Functor, typename Object, typename... Args>
+    constexpr auto invoke(Functor&& functor, Object&& object, Args&&... args)
+      ->  typename std::enable_if<
+            std::is_member_function_pointer<
+              typename std::decay<Functor>::type
+            >::value &&
+            !type_traits::is_target_reference<
+              Object&&,
+              typename std::decay<Functor>::type
+            >::value, 
+            decltype(((*std::forward<Object>(object)).*functor)(std::forward<Args>(args)...))
+          >::type
+    {
+      return ((*std::forward<Object>(object)).*functor)(std::forward<Args>(args)...);
+    }
+
+    template<typename Functor, typename Object>
+    constexpr auto invoke(Functor&& functor, Object&& object)
+      ->  typename std::enable_if<
+            std::is_member_object_pointer<
+              typename std::decay<Functor>::type
+            >::value &&
+            type_traits::is_target_reference<
+              Object&&,
+              typename std::decay<Functor>::type
+            >::value,
+            decltype(object.*functor)
+          >::type
+    {
+      return object.*functor;
+    }
+
+    template<typename Functor, typename Object>
+    constexpr auto invoke(Functor&& functor, Object&& object)
+      ->  typename std::enable_if<
+            std::is_member_object_pointer<
+              typename std::decay<Functor>::type
+            >::value &&
+            !type_traits::is_target_reference<
+              Object&&,
+              typename std::decay<Functor>::type
+            >::value, 
+            decltype((*std::forward<Object>(object)).*functor)
+          >::type
+    {
+      return (*std::forward<Object>(object)).*functor;
+    }
+
+    template<typename Functor, typename... Args>
+    constexpr auto invoke(Functor&& functor, Args&&... args)
+      ->  typename std::enable_if<
+            !std::is_member_pointer<
+              typename std::decay<Functor>::type
+            >::value,
+            decltype(std::forward<Functor>(functor)(std::forward<Args>(args)...))
+          >::type
+    {
+      return std::forward<Functor>(functor)(std::forward<Args>(args)...);
+    }
+  }
+}
+
+#endif //INVOKE_CPP11_HPP
