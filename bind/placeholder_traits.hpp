ac5c321aaaaaeba7fca0db3a625247324dc703f5
Tomasz Kamiński <tomaszkam@gmail.com>
2013-07-24 21:29:43 +0200

    Added placeholder traits and placeholder poistions.
    Added is_placeholder (that default to standard one) and placeholder_positions
    tratis, with default implementation for single argument placeholder.
    
    Added _all and _from<N> placeholder objects and specialized the
    new is_placeholder traits to use with them. The _from<N> should
    be changed to variable template if compiler will support that.
    Now using directive is used.

---
+++ bind/placeholder_traits.hpp
@@ -0,0 +1,23 @@
+#ifndef PLACEHOLDER_TRAITS_HPP
+#define PLACEHOLDER_TRAITS_HPP
+
+#include <functional>
+#include "integral_sequence.hpp"
+
+namespace functional
+{
+  template<typename T>
+  struct is_placeholder 
+   : std::is_placeholder<T> 
+  {};
+
+  template<typename Placeholder, std::size_t ArgCount>
+  struct placeholder_positions
+    : type_traits::integral_sequence<std::size_t, is_placeholder<Placeholder>::value>
+  {
+    static_assert(is_placeholder<Placeholder>::value, "Provided type must be an placeholder.");
+    static_assert(is_placeholder<Placeholder>::value <= ArgCount, "Placeholder position is greater the argument count.");
+  };
+}
+
+#endif //PLACEHOLDER_TRAITS_HPP
---
+++ bind/placeholders.hpp
@@ -0,0 +1,50 @@
+#ifndef PLACEHOLDERS_HPP
+#define PLACEHOLDERS_HPP
+
+#include <functional>
+#include <type_traits>
+#include "placeholder_traits.hpp"
+
+namespace functional
+{
+  struct all_placeholder {};
+
+  template<std::size_t N>
+  struct from_placeholder {};
+  
+  namespace placeholders
+  {
+    using namespace std::placeholders;
+
+    all_placeholder _all;
+
+    //Should be variable template
+    //But gcc 4.8.1 does not support it
+    template<std::size_t N>
+    using _from = from_placeholder<N>;
+  }
+
+  template<>
+  struct is_placeholder<all_placeholder>
+   : std::true_type
+  {};
+
+  template<std::size_t ArgCount>
+  struct placeholder_positions<all_placeholder, ArgCount>
+    : type_traits::make_integral_range<std::size_t, 1, ArgCount + 1>
+  {};
+
+  template<std::size_t N>
+  struct is_placeholder<from_placeholder<N>>
+    : std::true_type
+  {};
+
+  template<std::size_t N, std::size_t ArgCount>
+  struct placeholder_positions<from_placeholder<N>, ArgCount>
+    : type_traits::make_integral_range<std::size_t, N, ArgCount + 1>
+  {
+    static_assert(N <= ArgCount + 1, "To few argument provided.");
+  };
+}
+
+#endif //PLACEHOLDERS_HPP
