3408cb396fe6bb86bd8895a2dae5b015c1932f1b
Tomasz Kamiński <tomaszkam@gmail.com>
2014-08-16 09:59:39 +0200

    New implementation of make_integer_sequence
    Implemented function make_integer_sequence<T,N> in the way that
    complexity of compile time is O(log N).

--- bind/integer_sequence.hpp
+++ bind/integer_sequence.hpp
@@ -47,35 +47,64 @@ namespace type_traits
 
   template<typename... IntegralSequences>
   using concat = typename detail::concat_impl<IntegralSequences...>::type;
-  
+ 
   namespace detail
   {
-    template<typename T, T begin, T end, bool = (end - begin == 1)>
-    struct make_integer_range_impl
-    {
-      static constexpr T middle = (begin + end)/2;
-      using type = concat<typename make_integer_range_impl<T, begin, middle>::type,
-                          typename make_integer_range_impl<T, middle, end>::type>;
-    };
+    template<typename Sequence, typename Sequence::value_type v>
+    struct shift_impl;
 
-    template<typename T, T begin, T end>
-    struct make_integer_range_impl<T, begin, end, true>
+    template<typename T, T... values, T v>
+    struct shift_impl<integer_sequence<T, values...>, v>
     {
-      using type = integer_sequence<T, begin>;
+      using type = integer_sequence<T, values + v...>;
     };
+  }
 
-    template<typename T, T value>
-    struct make_integer_range_impl<T, value, value, false>
+  template<typename Sequence, typename Sequence::value_type v>
+  using shift = typename detail::shift_impl<Sequence, v>::type; 
+ 
+  namespace detail
+  {
+    template<typename T, T end, bool even = (end % 2 == 0), bool zero = (end / 2 == 0)>
+    struct make_integer_sequence_impl;
+
+    template<typename T, T end /*= 0*/>
+    struct make_integer_sequence_impl<T, end, true, true>
     {
       using type = integer_sequence<T>;
     };
+
+    template<typename T, T end /* = 1*/>
+    struct make_integer_sequence_impl<T, end, false, true>
+    {
+      using type = integer_sequence<T, 0>;
+    };
+
+    template<typename T, T end>
+    struct make_integer_sequence_impl<T, end, true, false>
+    {
+      static constexpr T middle = end / 2;
+      using left_part = typename make_integer_sequence_impl<T, middle>::type;
+      using rigth_part = shift<left_part, middle>;
+      using type = concat<left_part, rigth_part>;
+    };
+
+    template<typename T, T end>
+    struct make_integer_sequence_impl<T, end, false, false>
+    {
+      static constexpr T middle = end / 2;
+      using left_part = typename make_integer_sequence_impl<T, middle>::type;
+      using rigth_part = shift<left_part, middle+1>;
+      using type = concat<left_part, 
+                          integer_sequence<T, middle>,
+                          rigth_part>;
+    };
   } 
+  template<typename T, T end>
+  using make_integer_sequence = typename detail::make_integer_sequence_impl<T, end>::type;
 
   template<typename T, T begin, T end>
-  using make_integer_range = typename detail::make_integer_range_impl<T, begin, end>::type;
-
-  template<typename T, T end>
-  using make_integer_sequence = make_integer_range<T, 0, end>;
+  using make_integer_range = shift<make_integer_sequence<T, end-begin>, begin>;
 }
 
 #endif //INTEGER_SEQUENCE_HPP
